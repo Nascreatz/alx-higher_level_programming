3eb0e1903aa6ce97e23bc7e9f1260cfa22bcfdd7
Nascreatz <nascreat16@gmail.com>
2022-12-01 21:45:21 -0800

    Fixed __name__

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
-#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the addition of all arguments."""
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{:d}".format(a))
-#!/usr/bin/python3
 
 if __name__ == "__main__":
     from variable_load_5 import a
