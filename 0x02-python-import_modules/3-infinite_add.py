24a2a51d0f59ec6ef8886d92d6b9e91908297431
Nascreatz <nascreat16@gmail.com>
2022-12-01 20:33:53 -0800

    fixed __name__

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -9,3 +9,13 @@ if __name__ == "__main__":
         res += int(sys.argv[index])
         index += 1
     print("{:d}".format(res))
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
